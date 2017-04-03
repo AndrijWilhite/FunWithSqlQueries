864a991048b19165047683686f36c24bc0534569
Andrij Wilhitecd <andrij.wilhite@gmail.com>
2017-04-03 10:46:20 -0700

    first committ

---
+++ sql-exercises.sql
@@ -0,0 +1,60 @@
+--Write a query to return all category names with their descriptions from the Categories table.
+		--SELECT [CategoryName],[Description] FROM Categories
+--Write a query to return the contact name, customer id, company name and city name of all Customers in London
+		--SELECT[ContactName],[CustomerID],[CompanyName],[City] FROM Customers WHERE City='London'
+--Write a query to return all available columns in the Suppliers tables for the marketing managers and sales representatives that have a FAX number
+		--SELECT * From Suppliers WHERE Fax != 'NULL' AND  (ContactTitle ='Marketing Manager' OR ContactTitle ='Sales Representative')
+--Write a query to return a list of customer id's from the Orders table with required dates between Jan 1, 1997 and Dec 31, 1997 and with freight under 100 units.
+		--SELECT * From Orders WHERE RequiredDate BETWEEN '1997-1-1' AND '1997-12-31' AND Freight <= '100'
+--Write a query to return a list of company names and contact names of all customers from Mexico, Sweden and Germany.
+		--SELECT [CompanyName],[ContactName] FROM Customers WHERE (Country='Mexico' OR Country='Sweden' OR Country='Germany')
+--Write a query to return a count of the number of discontinued products in the Products table.
+		--SELECT COUNT(case Discontinued when '1' then 1 else null end) FROM Products 
+--Write a query to return a list of category names and descriptions of all categories beginning with 'Co' from the Categories table.
+		--SELECT [CategoryName],[Description] FROM Categories Where CategoryName Like 'Co%'
+--Write a query to return all the company names, city, country and postal code from the Suppliers table with the word 'rue' in their address. The list should ordered alphabetically by company name.
+		--SELECT [CompanyName],[City],[Country],[PostalCode] FROM Suppliers WHERE [Address] LIKE '%rue%'
+--Write a query to return the product id and the quantity ordered for each product labelled as 'Quantity Purchased' in the Order Details table ordered by the Quantity Purchased in descending order.
+		--SELECT [ProductID],[Quantity]AS 'Quantity Purchased' FROM [Order Details] ORDER BY [Quantity Purchased] DESC;
+--Write a query to return the company name, address, city, postal code and country of all customers with orders that shipped using Speedy Express, along with the date that the order was made.
+		--SELECT [CompanyName],[Address],[City],[PostalCode],[Country],[ShipName],[OrderDate] FROM Customers
+		--	inner join Orders ON Customers.CustomerID=Orders.CustomerID 
+		--	 WHERE ShipVia='1'
+--Write a query to return a list of Suppliers containing company name, contact name, contact title and region description.
+		--SELECT [CompanyName],[ContactName],[ContactTitle],[Region] FROM Suppliers
+--Write a query to return all product names from the Products table that are condiments.
+		--SELECT [ProductName] FROM Products
+		--	inner join Categories ON Categories.CategoryID=Products.CategoryID 
+		--		WHERE CategoryName='Condiments'
+--Write a query to return a list of customer names who have no orders in the Orders table.
+		--SELECT  CompanyName
+		--FROM    Customers
+		--WHERE   CustomerID NOT IN (SELECT CustomerID FROM Orders)
+--Write a query to add a shipper named 'Amazon' to the Shippers table using SQL.
+		--INSERT INTO Shippers(CompanyName)
+			--Values ('Amazon')
+--Write a query to change the company name from 'Amazon' to 'Amazon Prime Shipping' in the Shippers table using SQL.
+		--UPDATE Shippers
+		--SET CompanyName = 'Amazon Prime Shipping'
+		--WHERE CompanyName= 'Amazon'
+--Write a query to return a complete list of company names from the Shippers table. Include freight totals rounded to the nearest whole number for each shipper from the Orders table for those shippers with orders.
+		--SELECT [CompanyName], AVG([Freight]) as 'Average Freight Weight'
+		--FROM Shippers
+		--LEFT JOIN Orders ON Orders.ShipVia=Shippers.ShipperID
+		--group by CompanyName
+--Write a query to return all employee first and last names from the Employees table by combining the 2 columns aliased as 'DisplayName'. The combined format should be 'LastName, FirstName'.
+		--SELECT [LastName]+', '+[FirstName] AS FullName
+		--FROM employees
+--Write a query to add yourself to the Customers table with an order for 'Grandma's Boysenberry Spread'.
+			--INSERT INTO Customers(CustomerID,CompanyName)VALUES ('AIW','Andrij Wilhite');
+			--INSERT INTO  Orders(CustomerID)VALUES ((select CustomerID from Customers where CompanyName='Andrij Wilhite'));
+			--INSERT INTO [Order Details](OrderID,ProductID)
+			--VALUES ((SELECT OrderID from Orders join Customers ON Customers.CustomerID=Orders.CustomerID where CompanyName ='Andrij Wilhite'), (SELECT ProductID from Products where ProductName='Grandma''s Boysenberry Spread'))
+--Write a query to remove yourself and your order from the database.
+			--DELETE [Order Details] where OrderID=(select top 1 OrderID from Orders where CustomerID ='AIW ');
+			--DELETE Orders where CustomerID='AIW ';
+			--DELETE Customers where CompanyName='Andrij Wilhite';
+--Write a query to return a list of products from the Products table along with the total units in stock for each product. Include only products with TotalUnits greater than 100.
+		--SELECT [ProductName],[UnitsInStock]
+		--FROM Products
+		--WHERE UNitsInStock > 100
